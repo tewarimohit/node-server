53e1a3220b3ae240b9fcb14f60637ec5fb35c2da
tewarimohit <tewarimohit955@gmail.com>
2023-05-06 23:09:59 +0530

    added express

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+const express = require("express"); // returns a function which we execute
+const app = express();
+
+app.use((req, res, next) => {
+  // this middleware will execute for every incoming request through function, next is the function that is to be executed
+  // to allow the request travel to next middleware
+  console.log("in the middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("in the second middleware");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
