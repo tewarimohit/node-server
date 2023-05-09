88e7e57d3c2b166d364f3872ede2d7bfc8c0805d
tewarimohit <tewarimohit955@gmail.com>
2023-05-09 19:53:39 +0530

    pages added

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Welcome to page</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "products.html"));
 });
 
 module.exports = router;
